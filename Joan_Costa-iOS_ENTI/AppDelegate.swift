3836938d1121ed51b4e5cda7e33bc7e48a1c2d71
Joan Costa <jcosta@enti.cat>
2018-06-03 19:25:38 +0200

    Revert "Add Constrains for iPhone"
    This reverts commit fd5c411142460460f17b325d340331a2bbdf6f79.

--- Joan_Costa-iOS_ENTI/AppDelegate.swift
+++ Joan_Costa-iOS_ENTI/AppDelegate.swift
@@ -50,7 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         let userDefaults = UserDefaults.standard
-        //userDefaults.set("NO", forKey: "musicPlaying")
+        userDefaults.set("NO", forKey: "musicPlaying")
         userDefaults.set("NO", forKey: "mPlayerCreated")
     }
     
